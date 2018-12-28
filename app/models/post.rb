b9015ed01facd275c7e4b99befd40def5de2527d
morwen80 <ribesnero80@gmail.com>
2018-12-28 17:47:51 +0000

    Add Delete Comment option

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  has_many :comments
-  
+  has_many :comments, dependent: :destroy
+  # because when we delete a post we want to destroy all its comments as well
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 end
