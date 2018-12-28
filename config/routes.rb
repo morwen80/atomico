7d6ddcf6c89b3d8056cd0ef90ccb6524c594314d
morwen80 <ribesnero80@gmail.com>
2018-12-28 18:40:46 +0000

    Add Pages & About

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
     resources :comments
   end
   root "posts#index"
+
+get '/about', to: "pages#about" #it means that when we add the/about to the main url
+  # we will be directed to the "about" page in the pages folder. It's a static page
+
 end
 
 #COMMENTS are nested inside the post. If we go to rake routes
 # we will see that we will have paths like: /posts/:post-id/comments
 # because we will have different comments for every id/post. The comments belong to
-# post and there's no way to go to Comments without going to Posts first 
+# post and there's no way to go to Comments without going to Posts first
