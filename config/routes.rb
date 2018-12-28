48f61e4b03feff260d3286da68a07674671bbec5
morwen80 <ribesnero80@gmail.com>
2018-12-28 17:38:14 +0000

    Added comments for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root "posts#index"
 end
+
+#COMMENTS are nested inside the post. If we go to rake routes
+# we will see that we will have paths like: /posts/:post-id/comments
+# because we will have different comments for every id/post. The comments belong to
+# post and there's no way to go to Comments without going to Posts first 
